b5c02e90efc8e3743a840dc3110c1377ed6dde8f
Richard Kombo <richardkombo16@gmail.com>
2023-06-16 00:37:37 -0700

    Added:4-puts.c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
-#include<stdio.h>
- /*
-  * main:entry point
-  * Function used:puts
-  * Return value is 0
-  */
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
 int main(void)
 {
-puts("/"Programming is like a multilingual puzzle");
-return (0);
+	puts("Programming is like a multilingual puzzle");
+	return (0);
 }
