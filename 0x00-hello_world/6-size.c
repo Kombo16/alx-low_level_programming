8a3d2951f11d37f668b63128ec647cb0c7e4ada2
Richard Kombo <richardkombo16@gmail.com>
2023-06-16 06:38:40 -0700

    Added:file for outputing size of characters

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
 
-/*
+/**
  * main - Entry point of program
- * Return 0 (success)
+ * Return: 0 (success)
  */
-
 int main(void)
 {
-       	float f;
+	float f;
 	char c;
 	int i;
 	long int li;
 	long long int lli;
 
-	printf("float is %lu byte(s)\n",sizeof(f));
-	printf("char is %lu bytes\n",sizeof(c));
-	printf("int is %lu bytes\n",sizeof(i));
-	printf("long int is %lu bytes\n",sizeof(li));
-	printf("long long int is %lu bytes\n",sizeof(lli));
+	printf("Size of a char: %lu byte(s)\n", sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(lli));
+	printf("Size of a float: %lu byte(s)\n", sizeof(f));
 	return (0);
 }
