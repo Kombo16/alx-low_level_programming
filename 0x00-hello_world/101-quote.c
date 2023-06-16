3deb7c332bf82ac5f66191bb4ea09cb8b20e34c4
Richard Kombo <richardkombo16@gmail.com>
2023-06-16 11:26:53 -0700

    Added:File that prints c program to stderror

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point of program which uses write function
+ * Return: 1 -Success
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
