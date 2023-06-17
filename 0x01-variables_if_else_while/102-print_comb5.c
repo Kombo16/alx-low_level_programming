1b55343d11ad880d08c8bd8b56e7c36f7bdc6131
Richard Kombo <richardkombo16@gmail.com>
2023-06-17 02:46:13 -0700

    Added:102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Function to print different combination of two digits
+ * Return:0 success
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 48; i < 59; i++)
+	{
+		if (i != 58)
+		{
+			for (j = 48; j < 59; j++)
+			{
+				if (j != 58)
+				{
+					putchar(i);
+
+					putchar(j);
+
+					putchar(',');
+
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
