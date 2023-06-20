ffdaa0720571919e662b227a9fef6bb2fb2b5065
Richard Kombo <richardkombo16@gmail.com>
2023-06-20 14:42:39 -0700

    Added:0-putchar.c file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ *main - Function to print _putchar
+ *Return:0 success
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+README to describe C functions
