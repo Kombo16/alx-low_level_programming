6f2565f933d25adbc5e762b2f16c106e39104dae
Richard Kombo <richardkombo16@gmail.com>
2023-06-21 14:16:13 -0700

    Added:10-add.c file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *addition - Function to add two integers
+ *@a: First integer
+ *@b: Second integer
+ *Return: addition of both integers
+ */
+
+int addition(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}
