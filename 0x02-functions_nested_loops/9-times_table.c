05f22fd6d8f449302cc8b471627c1c505462c428
Richard Kombo <richardkombo16@gmail.com>
2023-06-21 13:42:22 -0700

    Added:9-times_table file

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * times_table - Function to print a 9-times table
+ * Return: Always 0
+ */
+
+void times_table(void)
+{
+	int i, j, re;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar('0');
+		for (j = 1; j <= 9; j++)
+		{
+			_putchar(',');
+			_putchar(' ');
+			re = i * j;
+			if (re <= 9)
+				_putchar('0' + re);
+			else
+			{
+				_putchar('0' + re / 10);
+				_putchar('0' + re % 10);
+			}
+		}
+		_putchar('\n');
+	}
+}
