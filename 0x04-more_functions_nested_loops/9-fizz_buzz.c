d41c9e65af66a7a7d234eeb0a8b0d119823e6213
Richard Kombo <richardkombo16@gmail.com>
2023-06-22 14:52:08 -0700

    Updated the fizzbuzz file

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
@@ -13,13 +12,21 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 != 0)
+		{
 			printf("Fizz");
+		}
 		else if (i % 5 == 0 && i % 3 != 0)
+		{
 			printf("Buzz");
+		}
 		else if (i % 3 == 0 && i % 5 == 0)
+		{
 			printf("FizzBuzz");
+		}
 		else
+		{
 			printf("%d", i);
+		}
 	}
 	printf("\n");
 	return (0);
