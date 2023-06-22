b92884a19276bfe172d17b217a6f4a15a81ac7b3
Richard Kombo <richardkombo16@gmail.com>
2023-06-21 17:41:27 -0700

    To update a file

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,10 +12,10 @@ void times_table(void)
 	for (i = 0; i <= 9; i++)
 	{
 		_putchar('0');
+		_putchar(' ');
 		for (j = 1; j <= 9; j++)
 		{
 			_putchar(',');
-			_putchar(' ');
 			_putchar(' ');
 
 			re = i * j;
