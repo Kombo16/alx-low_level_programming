f0c28674e13331546ca4acfde06a8fe1290b0b88
Richard Kombo <richardkombo16@gmail.com>
2023-07-03 14:10:36 -0700

    Added:a function to search for a string,locate a substring and printing a chessboard

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,1 +1,25 @@
+#include "main.h"
 
+/**
+ *_strpbrk - Function that searches a string for any set of bytes
+ *@s: Pointer to a character
+ *@accept: Pointer to a character
+ *Return: 0
+ *
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int k;
+
+	while (*s)
+	{
+		for (k = 0; accept[k]; k++)
+		{
+			if (*s == accept[k])
+				return (s);
+		}
+		s++;
+	}
+	return ('\0');
+}
