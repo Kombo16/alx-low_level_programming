f5324b1cfba3ee43d39508d25901fa458cc7b9d2
Richard Kombo <richardkombo16@gmail.com>
2023-07-04 12:14:55 -0700

    Added a crackme password

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *set_string - Function to set the value of pointer to a character
+ *@s:  pointer to a pointer
+ *@to: Character
+ *Return: 0
+ *
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
