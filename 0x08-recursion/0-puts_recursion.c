b0450fbe16e10f55a676ff5e657de8cab30b7ca7
Richard Kombo <richardkombo16@gmail.com>
2023-07-05 14:08:58 -0700

    Updated putchar file

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,7 +10,10 @@
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
-		return ('\n');
-	_putchar(*s);
-	return (_puts_recursion(s + 1));
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	_putchar('\n');
+	
 }
