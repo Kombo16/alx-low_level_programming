bcb13f4e8d33482d2d3f43e8dc31a844a9395390
Richard Kombo <richardkombo16@gmail.com>
2023-07-05 15:11:06 -0700

    Added:prime number and square root file

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+int actual_prime_number(int n, int i);
+
 /**
  *is_prime_number - Function that says if integer is a prime number
  *@n: number to evaluate
@@ -11,7 +13,7 @@ int is_prime_number(int n)
 {
 	if (n <= 1)
 		return (0);
-	return (is_prime_number(n, n - 1));
+	return (actual_prime_number(n, n - 1));
 }
 
 /**
