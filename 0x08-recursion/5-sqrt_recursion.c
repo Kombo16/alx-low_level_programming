94acfc3895ddd1faa737d53563307b3a4dde36ea
Richard Kombo <richardkombo16@gmail.com>
2023-07-05 14:55:38 -0700

    Added:prime number and square root file

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+int actual_sqrt_recursion(int n, int i);
+
+/**
+ *_sqrt_reqursion - Function that returns natural square root of number
+ *@n: number to calculate square root
+ *Return: resulting square root
+ *
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (actual_sqrt_recursion(n, 0));
+}
+
+/**
+ *actual_sqrt_recursion - Function that recurses to 
+ *find natural square root of number
+ *@n: number to calculate square root
+ *@i: iterator
+ *Return: resulting square root
+ *
+ */
+
+int actual_sqrt_recursion(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (actual_sqrt_conversion(n, i + 1));
+}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ *is_prime_number - Function that says if integer is a prime number
+ *@n: number to evaluate
+ *Return:1 if integer is prime number,0 if otherwise
+ *
+ */
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (actual_prime_number(n,n - 1));
+}
+
+/**
+ *actual_prime_number - Function that calculates if a number is prime recursively
+ *@n: number to evaluate
+ *@i: iterator
+ *Return:1 if n is prime 0 if not
+ *
+ *
+ */
+
+int actual_prime_number(int n, int i)
+{
+	if (i == 1)
+		return (1);
+	if (n % i == 0 && i > 0)
+		return (0);
+	return (actual_prime_number(n,i -1));
+}
