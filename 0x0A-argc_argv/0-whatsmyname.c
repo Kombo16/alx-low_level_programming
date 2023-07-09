8de0c6adbe30eee4c5d2e5ea6ef89859a22ce002
Richard Kombo <richardkombo16@gmail.com>
2023-07-09 14:32:28 -0700

    Updated all the files - a minor bug

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,8 +8,8 @@
  *Return:0 success
  */
 
-int main(int argc, char *argv[])
+int main(int argc__attribute__((unused)), char *argv[])
 {
-	printf("%s\n", argv[0]);
+	printf("%s\n", *argv);
 	return (0);
 }
