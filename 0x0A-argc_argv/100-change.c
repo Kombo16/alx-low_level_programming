3153e558d935bdf0a3ce53f56898ad91e5780329
Richard Kombo <richardkombo16@gmail.com>
2023-07-09 18:44:22 -0700

    Program to calculate change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- *main - main function to add integers
+ *main - main function to make change
  *@argc: argument count
  *@argv: argument vector
  *Return:0 success
@@ -10,25 +10,29 @@
 
 int main(int argc, char *argv[])
 {
-	int i;
-	int sum = 0;
+	int num, j;
+	int result = 0;
+	int coins[] = {25, 10, 5, 2, 1};
 
-	if (argc > 1)
+	if (argc != 2)
 	{
-		for (i = 1; i < argc; i++)
-		{
-			if ((argv[i] >= "A" && argv[i] <= "Z"))
-			{
-				printf("Error\n");
-				return (1);
-			}
-			sum += atoi(argv[i]);
-		}
-		printf("%d\n", sum);
+		printf("Error\n");
+		return (1);
 	}
-	else
+	num = atoi(argv[1]);
+	if (num < 0)
 	{
 		printf("0\n");
+		return (0);
 	}
+	for (j = 0; j < 5 && num >= 0; j++)
+	{
+		while (num >= coins[j])
+		{
+			result++;
+			num -= coins[j];
+		}
+	}
+	printf("%d\n", result);
 	return (0);
 }
