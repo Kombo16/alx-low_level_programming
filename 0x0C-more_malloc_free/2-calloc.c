5b40fd9b1ac0621444cb3b86c374f3bbee78c224
Richard Kombo <richardkombo16@gmail.com>
2023-07-13 16:16:15 -0700

    Updated calloc file......mismatching data types

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -5,18 +5,18 @@
 
 /**
  *_calloc -Function that allocates memory for an array
- *@nmember: elements of array
+ *@nmemb: elements of array
  *@size: size of bytes
  *Return: pointer to allocated memory
  *
  *
  */
 
-void *_calloc(unsigned int nmember, unsigned int size)
+void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	if (nmember == 0 || size == 0)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
-	int *alloc = (int *)malloc(nmember * size);
+	int *alloc = (int *)malloc(nmemb * size);
 
 	if (alloc == NULL)
 		return (NULL);
