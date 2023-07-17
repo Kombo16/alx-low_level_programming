fcdcbe04c2c989358adcc3f4596cf763553ebcfe
Richard Kombo <richardkombo16@gmail.com>
2023-07-17 11:40:51 -0700

    Added: Preprocessor files with the .h extension

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - Function that prints name of the file
+ *Return: 0 success
+ *
+ */
+
+int main(void)
+{
+	printf("%s\n", __BASE_FILE__);
+	return (0);
+}
