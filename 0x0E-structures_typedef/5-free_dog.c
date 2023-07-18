53f8441e7d45169184dc2a2c3e392f1b98ffb71b
Richard Kombo <richardkombo16@gmail.com>
2023-07-18 10:51:39 -0700

    Added file that frees dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ *free_dog - Function that frees dogs
+ *@d:Struct dog to free
+ *
+ *
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
