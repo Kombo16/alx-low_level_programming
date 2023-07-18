784316024f04807b146b7b8d3fb612da9537d5fd
Richard Kombo <richardkombo16@gmail.com>
2023-07-18 04:58:20 -0700

    Added:File that creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,13 +18,17 @@ dog_t *new_dog(char *name, float age, char *owner)
 	newDog = malloc(sizeof(dog_t));
 	if (newDog == NULL)
 		return (NULL);
-	newDog->name = malloc(sizeof(char) * (strlen(name) + 1));
-	newDog->owner = malloc(sizeof(char) * (strlen(owner) + 1));
-	if (newDog->name == NULL || newDog->owner == NULL)
+	newDog->name = malloc(sizeof(char) * ((strlen(name)) + 1));
+	if (newDog->name == NULL)
+	{
+		free(newDog);
+		return (NULL);
+	}
+	newDog->owner = malloc(sizeof(char) * ((strlen(owner)) + 1));
+	if (newDog->owner == NULL)
 	{
 		free(newDog);
 		free(newDog->name);
-		free(newDog->owner);
 		return (NULL);
 	}
 	strcpy(newDog->name, name);
