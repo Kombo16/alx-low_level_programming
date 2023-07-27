0d81d4e6a040cf6b6c6736ac8e0f383c4c5ea4b3
Richard Kombo <richardkombo16@gmail.com>
2023-07-27 16:53:31 -0700

    Added:Singly linked files

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -14,9 +14,9 @@ size_t print_list(const list_t *h)
 {
 	size_t s = 0;
 
-	while (h != NULL)
+	while (h)
 	{
-		if (h->str == NULL)
+		if (!h->str)
 			printf("[0] (nil)\n");
 		printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
