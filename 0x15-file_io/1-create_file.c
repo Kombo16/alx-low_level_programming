184d15364bd370477e49c16c5ccd019876a52fad
Richard Kombo <richardkombo16@gmail.com>
2023-08-11 15:14:37 -0700

    Fixed some errors

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,17 +13,18 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd;
 	ssize_t byteswritten;
-	size_t text_length;
+	int text_length;
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_RDWR | O_CREAT | O_NOCTTY | O_TRUNC,
+			S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 	if (text_content != NULL)
 	{
 		text_length = strlen(text_content);
-		byteswritten = write(STDIN_FILENO, text_content, text_length);
+		byteswritten = write(STDOUT_FILENO, text_content, text_length);
 		if (byteswritten == -1 || (size_t)byteswritten != text_length)
 		{
 			close(fd);
