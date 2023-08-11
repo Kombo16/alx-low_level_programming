5a5a1e920fef34e5395fc16f4f1c588d8f2948c1
Richard Kombo <richardkombo16@gmail.com>
2023-08-11 14:53:37 -0700

    Fixed some errors

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,26 +13,25 @@ int main(int argc, char **argv)
 		dprintf(2, "Usage: cp file_from file_to\n");
 		exit (98);
 	}
-	cp = copy_contents(argv[1], argv[2]);
+	char fd1 = argv[1];
+	char fd2 = argv[2];
+	ssize_t bytesread, byteswritten;
+	char buffer[BUFFER];
 
-	return(0);
-}
-/**
- *copy_contents - Function that copies contents
- *from one file to another
- *@filesrc: Source
- *@filedest: Destination
- *Return: 
- *
- */
-ssize_t copy_contents(const char *filesrc, const char *filedest)
-	int fp1;
-	int fp2;
-
-	fp1 = open(filesrc, O_WRONLY |  );
-	fp2 = open(filedest, O_RDWR | O_CREAT | O_TRUNC);
+	fd1 = open(filesrc, O_WRONLY);
+	if (fd1 == -1)
 	{
-
-
+		dprintf(STDERR_ FILENO, "Error: Can't read from file %s", fd1);
+		exit(98);
 	}
+	bytesread = read(fd1, buffer, BUFFER);
+
+	fd2 = open(filedest, O_RDWR | O_CREAT | O_TRUNC ,
+			O_IRUSR | O_IWUSR | O_IRGRP | O_IWGRP);
+	if (fd2 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s", fd2);
+		exit(98);
+	}
+	byteswritten = write(STDIN_FILENO, buffer, bytesread)
 }
