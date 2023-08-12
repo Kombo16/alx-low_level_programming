9d8f5ce2f77fca64fc29de190f5e73479d05e345
Richard Kombo <richardkombo16@gmail.com>
2023-08-11 19:26:17 -0700

    Fixed some errors

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,28 +10,49 @@ int main(int argc, char **argv)
 {
 	if (argc != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit (98);
 	}
-	char fd1 = argv[1];
-	char fd2 = argv[2];
+	int fd1;
+	int fd2;
+	char *file_from = argv[1];
+	char *file_to = argv[2];
 	ssize_t bytesread, byteswritten;
 	char buffer[BUFFER];
 
-	fd1 = open(filesrc, O_WRONLY);
-	if (fd1 == -1)
+	fd1 = open(file_from, O_RDONLY);
+	bytesread = read(fd1, buffer, BUFFER);
+	if (fd1 == -1 &&  bytesread == -1)
 	{
-		dprintf(STDERR_ FILENO, "Error: Can't read from file %s", fd1);
+		dprintf(STDERR_ FILENO, "Error: Can't read from file %s\n", fd1);
 		exit(98);
 	}
-	bytesread = read(fd1, buffer, BUFFER);
-
-	fd2 = open(filedest, O_RDWR | O_CREAT | O_TRUNC ,
+	fd2 = open(fd2, O_WR0NLY | O_CREAT | O_TRUNC ,
 			O_IRUSR | O_IWUSR | O_IRGRP | O_IWGRP);
 	if (fd2 == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s", fd2);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", fd2);
 		exit(98);
 	}
-	byteswritten = write(STDIN_FILENO, buffer, bytesread)
+	byteswritten = write(STDIN_FILENO, buffer, bytesread);
+	while (bytesread > 0)
+	{
+		if (byteswritten == -1 || byteswritten != bytesread)
+		{
+			dprintf(STDERR_FILENO,"Error: Can't write to %s", fd2);
+			exit(99);
+		}
+
+	}
+	if (close(fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
+		exit(100);
+	}
+	if (close(fd2) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
+		exit(100);
+	}
+	return (0);
 }
