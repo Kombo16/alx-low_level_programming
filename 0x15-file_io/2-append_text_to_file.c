f2052f45a3221bc38d6c4120e74fc4a066205a9b
Richard Kombo <richardkombo16@gmail.com>
2023-08-11 18:49:40 -0700

    Fixed some errors

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,6 +18,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
+	if (text_content != NULL)
 	text_length = strlen(text_content);
 	byteswritten = write(fd, text_content, text_length);
 	if (byteswritten == -1 || (size_t)byteswritten != text_length)
